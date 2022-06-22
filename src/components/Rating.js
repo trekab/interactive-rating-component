14372f15a7b7ed1ca7aaed88124456f50239759a
Treasure Kabareebe <treasurecalm@gmail.com>
2022-06-22 03:12:52 +0300

    feat: Add the hover effects and active states for the elements

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -13,19 +13,61 @@ const Rating = () => {
           Please let us know how we did with your support request. All feedback
           is appreciated to help us improve our offering!
         </p>
-        <div>
-          <input type="radio" id="one" name="rating" value="1" />
-          <label for="one">1</label>
-          <input type="radio" id="two" name="rating" value="2" />
-          <label for="two">2</label>
-          <input type="radio" id="three" name="rating" value="3" />
-          <label for="three">3</label>
-          <input type="radio" id="four" name="rating" value="4" />
-          <label for="four">4</label>
-          <input type="radio" id="five" name="rating" value="5" />
-          <label for="five">5</label>
+        <div className="radio-buttons">
+          <div>
+            <input
+              type="radio"
+              id="one"
+              name="rating"
+              value="1"
+              className="radio-btn"
+            />
+            <label htmlFor="one">1</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="two"
+              name="rating"
+              value="2"
+              className="radio-btn"
+            />
+            <label htmlFor="two">2</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="three"
+              name="rating"
+              value="3"
+              className="radio-btn"
+            />
+            <label htmlFor="three">3</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="four"
+              name="rating"
+              value="4"
+              className="radio-btn"
+            />
+            <label htmlFor="four">4</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="five"
+              name="rating"
+              value="5"
+              className="radio-btn"
+            />
+            <label htmlFor="five">5</label>
+          </div>
         </div>
-        <button type="submit" className="submit-btn">SUBMIT</button>
+        <button type="submit" className="submit-btn">
+          SUBMIT
+        </button>
       </form>
     </div>
   );
